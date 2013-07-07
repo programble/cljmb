2feec3212f1bf3b836bd340cc222eccbf741550c
Curtis McEnroe <programble@gmail.com>
2013-07-07 17:05:20 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject cljmb "0.1.0-SNAPSHOT"
+  :description "MusicBrainz API client"
+  :url "https://github.com/programble/cljmb"
+  :license {:name "ISC License"
+            :url "http://www.isc.org/software/license"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.4"]
+                 [cheshire "5.2.0"]])
---
+++ src/cljmb/cljmb.clj
@@ -0,0 +1,4 @@
+(ns cljmb.core)
+
+(defn foo []
+  "foo")
